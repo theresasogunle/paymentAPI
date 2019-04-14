f45745f2b6b1d77537b39651a7dbfc6074c8f3a8
theresasogunle <sogunledolapo@gmail.com>
2019-04-14 14:19:34 +0100

    changed rave url

--- src/functions/rave.ts
+++ src/functions/rave.ts
@@ -1,5 +1,5 @@
 export default () => {
-  let BaseURL = process.env.RAVE_BASEURL;
+  let BaseURL = process.env.RAVE_TESTURL;
   if (process.env.RAVE_ENV.toLowerCase() === "live") {
     BaseURL = process.env.RAVE_LIVEURL;
   }
